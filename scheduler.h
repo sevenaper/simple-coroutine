7a7735a4689dd4910bdc719b610f289a9b45f2df
Hiram Liu <liuxin@imo.im>
2020-07-10 00:21:28 -0700

    diaoyu

--- scheduler.h
+++ scheduler.h
@@ -1,9 +1,18 @@
 #pragma once
 #include <memory>
+#include <vector>
+#include <list>
+#include <iostream>
+
 #include "fiber.h"
-#include "mutex.h"
+#include "thread.h"
 class Scheduler
 {
-    public:
+public:
     typedef std::shared_ptr<Scheduler> ptr;
+    typedef Mutex MutexType;
+    Scheduler(size_t threads = 1, bool use_caller = true, const std::string& name = "");
+    virtual ~Scheduler();
+    const std::string& getName()const { return m_name; }
+
 };
